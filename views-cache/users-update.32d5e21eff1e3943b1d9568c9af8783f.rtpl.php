e330f981b888ea1eddb7f2be6687ef0cc3f4f94d
Paulo Spiguel <41386612+PauloSpiguel@users.noreply.github.com>
2018-10-09 16:56:39 -0300

    Correção update-users

--- views-cache/users-update.32d5e21eff1e3943b1d9568c9af8783f.rtpl.php
+++ views-cache/users-update.32d5e21eff1e3943b1d9568c9af8783f.rtpl.php
@@ -24,50 +24,53 @@
                    <label for="desperson">Nome</label>
                    <input type="text" class="form-control" id="desperson" name="desperson" placeholder="Digite o nome" value="<?php echo htmlspecialchars( $user["desperson"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
                </div>
-              <div class="form-group">
-                   <label for="destypedoc">Tipo Documento</label>               
-              <select name="destypedoc" id="destypedoc" >
-                    <option>Selecione...</option>
-                     <option id="rg" value="rg" <?php if( $user["destypedoc"] == 'rg' ){ ?>selected<?php } ?>>RG</option>
-                     <option value="cpf" <?php if( $user["destypedoc"] == 'cpf' ){ ?>selected<?php } ?>>CPF</option>
-                     <option value="passaporte" <?php if( $user["destypedoc"] == 'passaporte' ){ ?>selected<?php } ?>>Passaporte</option>                
-              </select> 
-         </div>
-         <div class="form-group">
-           <label for="nrdocument">Número Documento</label>
-           <input type="number" class="form-control" id="nrdocument" name="nrdocument" placeholder="Digite o Nº do Doumento" value="<?php echo htmlspecialchars( $user["nrdocument"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
-       </div>
-       <div class="form-group" id="hidden_div" style="display: none;">
-           <label for="nrdocument">Orgão Emissor</label>
-           <input type="text" name="nrdocument" id="nrdocument" placeholder="Digite o Orgão Emissor" value="<?php echo htmlspecialchars( $user["desemitter"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
-       </div>
-       <div class="form-group">
-        <label for="deslogin">Login</label>
-        <input type="text" class="form-control" id="deslogin" name="deslogin" placeholder="Digite o login"  value="<?php echo htmlspecialchars( $user["deslogin"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
+               <div class="form-group">
+                   <label for="destypedoc">Tipo Documento</label></br>              
+                   <select name="destypedoc" id="destypedoc" >
+                      <option>Selecione...</option>
+                      <option id="RG" value="RG" <?php if( $user["destypedoc"] == 'RG' ){ ?>selected<?php } ?>>RG</option>
+                      <option value="CPF" <?php if( $user["destypedoc"] == 'CPF' ){ ?>selected<?php } ?>>CPF</option>
+                      <option value="Passaporte" <?php if( $user["destypedoc"] == 'Passaporte' ){ ?>selected<?php } ?>>Passaporte</option>                
+                  </select> 
+              </div>
+              <div style="display: inline-flex;">
+                  <div class="form-group">
+                     <label for="nrdocument">Número Documento</label>
+                     <input type="number" style="width:100%" class="form-control" id="nrdocument" name="nrdocument" placeholder="Digite o Nº do Doumento" value="<?php echo htmlspecialchars( $user["nrdocument"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
+                 </div>
+                 <div class="form-group" id="hidden_div" style="display: none; margin-left: 110px; border: 2px solid #ff0 ">
+                     <label for="desemitter">Orgão Emissor</label>
+                     <input type="text" class="form-control" name="desemitter" id="desemitter" placeholder="Digite o Orgão Emissor" value="<?php echo htmlspecialchars( $user["desemitter"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
+                 </div>
+             </div>
+
+             <div class="form-group">
+              <label for="deslogin">Login</label>
+              <input type="text" class="form-control" id="deslogin" name="deslogin" placeholder="Digite o login"  value="<?php echo htmlspecialchars( $user["deslogin"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
+          </div>
+          <div class="form-group">
+              <label for="nrphone">Telefone</label>
+              <input type="tel" class="form-control" id="nrphone" name="nrphone" placeholder="Digite o telefone"  value="<?php echo htmlspecialchars( $user["nrphone"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
+          </div>
+          <div class="form-group">
+              <label for="desemail">E-mail</label>
+              <input type="email" class="form-control" id="desemail" name="desemail" placeholder="Digite o e-mail" value="<?php echo htmlspecialchars( $user["desemail"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
+          </div>
+          <div class="form-group">
+              <label for="inadmin">Perfil</label></br>
+              <select name="inadmin" id="inadmin">
+                <option style="display: none" value="0">Master</option>
+                <option value="1" <?php if( $user["inadmin"] == 1 ){ ?>selected<?php } ?>>Administrador</option>
+                <option value="2" <?php if( $user["inadmin"] == 2 ){ ?>selected<?php } ?>>Ouvidor</option>
+                <option value="3" <?php if( $user["inadmin"] == 3 ){ ?>selected<?php } ?>>Cidadão</option>
+                <option value="4" <?php if( $user["inadmin"] == 4 ){ ?>selected<?php } ?>>Consulta</option>
+            </select>          
+        </div>
     </div>
-    <div class="form-group">
-        <label for="nrphone">Telefone</label>
-        <input type="tel" class="form-control" id="nrphone" name="nrphone" placeholder="Digite o telefone"  value="<?php echo htmlspecialchars( $user["nrphone"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
-    </div>
-    <div class="form-group">
-        <label for="desemail">E-mail</label>
-        <input type="email" class="form-control" id="desemail" name="desemail" placeholder="Digite o e-mail" value="<?php echo htmlspecialchars( $user["desemail"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
-    </div>
-    <div class="form-group">
-        <label for="inadmin">Perfil</label></br>
-        <select name="inadmin" id="inadmin">
-          <option style="display: none" value="0">Master</option>
-          <option value="1" <?php if( $user["inadmin"] == 1 ){ ?>selected<?php } ?>>Administrador</option>
-          <option value="2" <?php if( $user["inadmin"] == 2 ){ ?>selected<?php } ?>>Ouvidor</option>
-          <option value="3" <?php if( $user["inadmin"] == 3 ){ ?>selected<?php } ?>>Cidadão</option>
-          <option value="4" <?php if( $user["inadmin"] == 4 ){ ?>selected<?php } ?>>Consulta</option>
-      </select>          
-  </div>
-</div>
-<!-- /.box-body -->
-<div class="box-footer">
-   <button type="submit" class="btn btn-primary">Salvar</button>
-</div>
+    <!-- /.box-body -->
+    <div class="box-footer">
+     <button type="submit" class="btn btn-primary">Salvar</button>
+ </div>
 </form>
 </div>
 </div>
@@ -80,7 +83,7 @@
 <script>
    window.onload=function(){
       document.getElementById('destypedoc').addEventListener('change', function () {
-        var style = this.value == 'rg' ? 'block' : 'none';
+        var style = this.value == 'RG' ? 'block' : 'none';
         document.getElementById('hidden_div').style.display = style;
     });
   }
